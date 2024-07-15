600445d5617a6729d46e57bfb146e17cc060999b
devhong <popodaddy@gmail.com>
2024-07-15 22:21:19 +0900

    Input 컴포넌트 작업

--- src/App.tsx
+++ src/App.tsx
@@ -9,26 +9,22 @@ import { top3BoardListMock } from "mocks";
 import Top3Item from "components/Top3Item";
 import CommentItem from "components/commentItem";
 import FavoriteItem from "components/favoriteItem";
+import InputBox from "components/inputbox";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState<string>("");
+
   return (
     <>
-      <div
-        style={{
-          padding: "0 20px",
-          display: "flex",
-          flexDirection: "row",
-          columnGap: "30px",
-          rowGap: "20px",
-        }}
-      >
-        {/* {commentItemListMock.map((item) => (
-          <CommentItem commentItemList={item} />
-        ))} */}
-        {favoriteListMock.map((item) => (
-          <FavoriteItem favoriteListItem={item} />
-        ))}
-      </div>
+      <InputBox
+        label="이메일"
+        type="text"
+        placeholder="이메일 주소를 입력해주세요."
+        value={value}
+        error={false}
+        setValue={setValue}
+      />
     </>
   );
 }
